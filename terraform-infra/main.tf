870fbe3ec83ab38e7778b84e1f0334fd6f7b9819
mruperez <mruperez@paradigmadigital.com>
2025-04-13 13:40:01 +0200

    empiezo con el alb y el asg, reestructuro un poco la carpeta y añado más extensiones en .gitignore

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -1,3 +1,16 @@
 module "vpc" {
   source = "./000-VPC-y-subredes"
 }
+
+module "alb" {
+  source           = "./010-ALB"
+  vpc_id           = module.vpc.vpc_id
+  public_subnet_id = module.vpc.public_subnet_id
+}
+
+module "asg" {
+  source           = "./020-ec2-ASG"
+  vpc_id           = module.vpc.vpc_id
+  public_subnet_id = module.vpc.public_subnet_id
+  nat_instance_id  = module.vpc.nat_instance_id
+}
