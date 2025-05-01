3dcb97546903892c5d7bd64e098b1d60ceea895c
mruperez <mruperez@paradigmadigital.com>
2025-05-01 14:35:00 +0200

    añado listener al ALB, infra funcionando

--- terraform/010-ALB/main.tf
+++ terraform/010-ALB/main.tf
@@ -22,6 +22,17 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
 resource "aws_lb" "app_alb" {
   name = "tfg-alb"
   internal = false
